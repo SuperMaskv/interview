55143956d1df9546951f806364e891b262e2af2f
SuperMaskv <supermaskv@github.com>
2023-05-29 13:20:06 +0800

    增加小和问题

---
+++ algorithm/system-class/src/main/java/com/github/supermaskv/chaptor4/SmallSum.java
@@ -0,0 +1,12 @@
+package com.github.supermaskv.chaptor4;
+
+/**
+ * @author supermaskv
+ *
+ * 求每个数左侧小于这个数的和
+ */
+public class SmallSum {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ algorithm/system-class/src/main/java/com/github/supermaskv/chaptor4/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * 归并排序及其相关面试题
+ */
+package com.github.supermaskv.chaptor4;
