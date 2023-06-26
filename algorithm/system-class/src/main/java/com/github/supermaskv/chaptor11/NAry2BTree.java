ceeba4b7eeea25d61b96db5ba9b7e3e0e675174c
SuperMaskv <supermaskv@foxmail.com>
2023-06-26 10:54:45 +0800

    完成leetcode 431

--- algorithm/system-class/src/main/java/com/github/supermaskv/chaptor11/NAry2BTree.java
+++ algorithm/system-class/src/main/java/com/github/supermaskv/chaptor11/NAry2BTree.java
@@ -1,5 +1,6 @@
 package com.github.supermaskv.chaptor11;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -8,7 +9,19 @@ import java.util.List;
  */
 public class NAry2BTree {
     public static void main(String[] args) {
-
+        Node nAryRoot = new Node(0);
+        nAryRoot.children = new ArrayList<>();
+        nAryRoot.children.add(new Node(1));
+        nAryRoot.children.add(new Node(2));
+        nAryRoot.children.add(new Node(3));
+        nAryRoot.children.get(0).children = new ArrayList<>();
+        nAryRoot.children.get(0).children.add(new Node(4));
+        nAryRoot.children.get(0).children.add(new Node(5));
+        nAryRoot.children.get(0).children.add(new Node(6));
+        Codec codec = new Codec();
+        TreeNode btreeRoot = codec.encode(nAryRoot);
+        Node newNAryRoot = codec.decode(btreeRoot);
+        System.out.println();
     }
 
     private static class Node {
@@ -40,11 +53,46 @@ public class NAry2BTree {
 
     private static class Codec {
         public TreeNode encode(Node root) {
-            return null;
+            if (root == null) return null;
+            TreeNode btreeRoot = new TreeNode(root.val);
+            btreeRoot.left = en(root);
+            return btreeRoot;
+        }
+
+        private TreeNode en(Node root) {
+            if (root == null || root.children == null) return null;
+            Node nAryHead = root.children.get(0);
+            TreeNode head = new TreeNode(nAryHead.val);
+            head.left = en(nAryHead);
+            TreeNode p = head;
+            for (int i = 1; i < root.children.size(); i++) {
+                Node node = root.children.get(i);
+                TreeNode treeNode = new TreeNode(node.val);
+                treeNode.left = en(node);
+                p.right = treeNode;
+                p = treeNode;
+            }
+            return head;
         }
 
         public Node decode(TreeNode root) {
-            return null;
+            if (root == null) return null;
+            Node ans = new Node(root.val);
+            ans.children = de(root);
+            return ans;
+        }
+
+        private List<Node> de(TreeNode root) {
+            if (root == null || root.left == null) return null;
+            List<Node> children = new ArrayList<>();
+            TreeNode btreeHead = root.left;
+            while (btreeHead != null) {
+                Node node = new Node(btreeHead.val);
+                node.children = de(btreeHead);
+                children.add(node);
+                btreeHead = btreeHead.right;
+            }
+            return children;
         }
     }
 }
